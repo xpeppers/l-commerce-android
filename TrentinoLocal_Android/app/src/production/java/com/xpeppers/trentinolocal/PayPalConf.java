8e4c9e13152d722072d5e53191d17605ec776960
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-12-16 09:52:43 +0100

    Using sandbox in production flavour (this must be changed when we go live)

--- TrentinoLocal_Android/app/src/production/java/com/xpeppers/trentinolocal/PayPalConf.java
+++ TrentinoLocal_Android/app/src/production/java/com/xpeppers/trentinolocal/PayPalConf.java
@@ -3,7 +3,7 @@ import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 public class PayPalConf {
 
-    public final static String MODE = PayPalConfiguration.ENVIRONMENT_PRODUCTION;
+    public final static String MODE = PayPalConfiguration.ENVIRONMENT_SANDBOX;
     public final static String API_KEY = "Af12_vh0I5NBtn1YH6upOVu6aytPJnfL1YgCEfPYwxrm_hHuEhGyg7GTZ8oy7g3uU3w41PCMeID7mRV5";
 
 }
