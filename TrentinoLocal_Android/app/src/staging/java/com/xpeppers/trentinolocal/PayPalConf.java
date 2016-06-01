d7f2eb0ef01eb942ad31578da43a3420fb4617da
linnal <erindajaupi@gmail.com>
2016-06-01 10:48:04 +0200

    disable credit card payments

--- TrentinoLocal_Android/app/src/staging/java/com/xpeppers/trentinolocal/PayPalConf.java
+++ TrentinoLocal_Android/app/src/staging/java/com/xpeppers/trentinolocal/PayPalConf.java
@@ -6,5 +6,6 @@ public class PayPalConf {
 
     public final static String MODE = PayPalConfiguration.ENVIRONMENT_SANDBOX;
     public final static String APP_ID = "AQQUkVb65LNxGCR6enWzS7kbaf99AMg2lNHL1jWRaRJOSOfqczSgJio06EXSlHDoWmNw8_loQfRU2_2V";
+    public final static boolean AcceptCreditCards = false;
 
 }
