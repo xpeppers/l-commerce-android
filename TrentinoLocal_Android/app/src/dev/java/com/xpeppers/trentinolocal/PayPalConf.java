40306143e95dc72c9b2864e25526ad935bfb0393
tiagomartinho <tiago.martinho@xpeppers.com>
2016-09-28 10:55:17 +0200

    adds missing constants, upgrades gradle

--- TrentinoLocal_Android/app/src/dev/java/com/xpeppers/trentinolocal/PayPalConf.java
+++ TrentinoLocal_Android/app/src/dev/java/com/xpeppers/trentinolocal/PayPalConf.java
@@ -3,8 +3,7 @@ package com.xpeppers.trentinolocal;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 
 public class PayPalConf {
-
     public final static String MODE = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
     public final static String APP_ID = "AQQUkVb65LNxGCR6enWzS7kbaf99AMg2lNHL1jWRaRJOSOfqczSgJio06EXSlHDoWmNw8_loQfRU2_2V";
-
+    public static boolean AcceptCreditCards = true;
 }
